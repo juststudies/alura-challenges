02abb68bf2264cfbfdb2049c8854683be24df756
Nicolas Codonho Scorpelini <50plantasquemudaramahistoria@gmail.com>
2021-05-30 13:49:12 -0300

    ajeitado o search icon

--- js/handleClicks.js
+++ js/handleClicks.js
@@ -1,6 +1,7 @@
 const burgerImage = document.querySelector('.burger-menu');
 const burgerMenu = document.querySelector('.burger-menu');
 const nav = document.querySelector('.container nav');
+const searchIcon = document.querySelector('.search-icon');
 
 burgerImage.addEventListener('click', ()=>{
     if (burgerImage.querySelector('img').src.indexOf('burger-icon')!=-1) {
@@ -13,4 +14,25 @@ burgerImage.addEventListener('click', ()=>{
 
 burgerMenu.addEventListener('click', ()=>{
     nav.classList.toggle('menu__responsive');
-})
+});
+
+searchIcon.addEventListener('click', (ev)=>{
+    document.querySelector('.logo').style.display = 'none';
+    searchIcon.querySelector('img').style.display = 'none';
+    document.querySelector('.search-box').style.display = 'block';
+    document.querySelector('.search-box').style.position = 'absolute';
+    document.addEventListener('keyup', (ev)=>{
+        if(ev.key === "Escape"){
+            removeStyles();
+            console.log('disparou!');
+        }
+    });
+});
+
+searchIcon.closest('header').addEventListener('focusout', removeStyles);
+
+function removeStyles(){
+    document.querySelector('.logo').removeAttribute('style');
+    searchIcon.querySelector('img').removeAttribute('style');
+    document.querySelector('.search-box').removeAttribute('style');
+}
